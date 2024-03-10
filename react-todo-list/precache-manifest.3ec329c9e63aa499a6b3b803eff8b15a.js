b0a7bd4dca0ff9762469aaed69b8b49cc26ce786
viper-n <github@mailsecure.mozmail.com>
2024-03-09 23:38:34 -0500

    Add files via upload

---
+++ react-todo-list/precache-manifest.3ec329c9e63aa499a6b3b803eff8b15a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b29dc42db913eb75162",
+    "url": "/static/css/main.e6948fec.chunk.css"
+  },
+  {
+    "revision": "2b29dc42db913eb75162",
+    "url": "/static/js/main.227f6613.chunk.js"
+  },
+  {
+    "revision": "43641116dc8ac65838bf",
+    "url": "/static/js/runtime~main.f878ed7b.js"
+  },
+  {
+    "revision": "480f09dfcb0369c0da2f",
+    "url": "/static/js/2.dc5b7cc3.chunk.js"
+  },
+  {
+    "revision": "a59f633a310fb2c09560",
+    "url": "/static/js/3.f995e34b.chunk.js"
+  },
+  {
+    "revision": "149abc20f20051551f9549949b24615c",
+    "url": "/index.html"
+  }
+];
